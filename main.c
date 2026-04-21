42145a0b0c0ec6eecda915017880b5be8a1bd2bb
Aryem03 <aryemzitoune@gmail.com>
2026-04-21 17:53:39 +0200

    petit bug au niveau de la compilation depuis mon ordi A.Z

--- main.c
+++ main.c
@@ -3,25 +3,39 @@
 
 void modeEnseignant(){
 	printf("\n --- Bienvenue dans le mode Enseignant ---\n");
-	printf("Dans ce mode on va creer les questions\n");
+	printf("Dans ce mode on va creer les questions \n");
 }
 
 
 void modeEtudiant(){
 	printf("\n--- Bienvenue dans le mode Etudiant ---\n");
-	printf("Prépare toi pour le quiz\n");
+	printf("C'est parti pour le quiz \n");
 }
 
 int main(){
 
 int choix = 0;
-	while( choix != 0){
+	while( choix != 3){
 	printf("\n--- Bienvenue dans QUESTION POUR UN TAKEN ---\n");
 	printf("1. Mode Enseignant\n");
 	printf("2. Mode Etudiant\n");
 	printf("3. Quitter\n");
 	printf("Quel est votre choix:  \n");
 	scanf("%d",&choix);
+
+	if(choix == 1){
+		modeEnseignant();
+}
+	else if(choix == 2){
+		modeEtudiant();
+}
+	else if(choix == 3){
+		printf("Au revoir ! \n");
+}
+	else{
+		printf("choix invalide recommancez\n");
+}
+
 
 	}
 
